255431c6cf01c62fb6eb0b9b104cf74b995f07b0
hsmartineza <hsmartineza@unal.edu.co>
2018-05-10 15:56:32 -0500

    Patrones de diseño
    Clase Ingesoft II Mayo 10 2018

---
+++ src/pattern/PooledObject.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pattern;
+
+
+import java.util.concurrent.ExecutorService;
+        
+        
+
+/**
+ *
+ * @author Estudiante
+ */
+
+
+public class PooledObject {
+	public String temp1;
+	public String temp2;
+	public String temp3;
+	
+	public String getTemp1() {
+		return temp1;
+	}
+	public void setTemp1(String temp1) {
+		this.temp1 = temp1;
+	}
+	public String getTemp2() {
+		return temp2;
+	}
+	public void setTemp2(String temp2) {
+		this.temp2 = temp2;
+	}
+	public String getTemp3() {
+		return temp3;
+	}
+	public void setTemp3(String temp3) {
+		this.temp3 = temp3;
+	}
+	
+	
+}
